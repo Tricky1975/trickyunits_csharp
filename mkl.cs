6b68fc561f02aaafb04dceb0da6c2e42dd975419
Tricky1975 <facebook@tbbs.nl>
2018-08-24 09:24:06 +0200

    MKL start

---
+++ mkl.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace TrickyUnits{
+class MKL{
+	static SortedDictionary<string,string> VERSIONS=new SortedDictionary<string,string>;
+	static SortedDictionary<string,string> LICENSES=new SortedDictionary<string,string>;
+	// TODO: Get the actual functions/methods up.
+}
