2c2096b58e418eee4f9dd1641a05dce53b2bda8d
Tricky1975 <facebook@tbbs.nl>
2018-08-24 16:54:22 +0200

    License blocks added

--- FileList.cs
+++ FileList.cs
@@ -1,3 +1,22 @@
+// Lic:
+//   FileList.cs
+//   Quick File List
+//   version: 18.08.24
+//   Copyright (C) 2018 Jeroen P. Broks
+//   This software is provided 'as-is', without any express or implied
+//   warranty.  In no event will the authors be held liable for any damages
+//   arising from the use of this software.
+//   Permission is granted to anyone to use this software for any purpose,
+//   including commercial applications, and to alter it and redistribute it
+//   freely, subject to the following restrictions:
+//   1. The origin of this software must not be misrepresented; you must not
+//      claim that you wrote the original software. If you use this software
+//      in a product, an acknowledgment in the product documentation would be
+//      appreciated but is not required.
+//   2. Altered source versions must be plainly marked as such, and must not be
+//      misrepresented as being the original software.
+//   3. This notice may not be removed or altered from any source distribution.
+// EndLic
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
@@ -59,8 +78,8 @@ namespace TrickyUnits
         }
 
         static FileList(){
-            MKL.Version("FL", "FL");
-            MKL.Lic("FL", "FL");
+            MKL.Version("Tricky Units for C# - FileList.cs","18.08.24");
+MKL.Lic    ("Tricky Units for C# - FileList.cs","ZLib License");
         }
     }
 }
