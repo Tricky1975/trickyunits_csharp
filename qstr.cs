6a1065cd0ec2e08bd4c44e5d6f8a2d623c9f2e64
Tricky1975 <facebook@tbbs.nl>
2018-09-09 14:50:32 +0200

    Advanced qstr

--- qstr.cs
+++ qstr.cs
@@ -1,7 +1,7 @@
 // Lic:
 //   qstr.cs
 //   Quick String Functions
-//   version: 18.08.31
+//   version: 18.09.09
 //   Copyright (C) 2018 Jeroen P. Broks
 //   This software is provided 'as-is', without any express or implied
 //   warranty.  In no event will the authors be held liable for any damages
@@ -20,11 +20,15 @@
 
 namespace TrickyUnits
 {
+    /// <summary>
+    /// Just some quick functions to use with strings.
+    /// Nothing special :P
+    /// </summary>
     class qstr
     {
         static qstr()
         {
-            MKL.Version("Tricky Units for C# - qstr.cs","18.08.31");
+            MKL.Version("Tricky Units for C# - qstr.cs","18.09.09");
             MKL.Lic    ("Tricky Units for C# - qstr.cs","ZLib License");
         }
 
@@ -47,5 +51,20 @@ namespace TrickyUnits
         public static int Len(string s) =>  s.Length;  // The only reason why I put this one in, was for quick translations from BlitzMax.
         public static string MyTrim(string s) => s.Trim(); // The only reason why I put this one in, was for Quick translations from Go, where I used this function, as the Trim feature in Go was impractical.
         public static string Chr(int i) => ((char)i).ToString(); // The C# method is just impractical. Sorry!
+        public static string Upper(string s) => s.ToUpper(); // BlitzMax conversion
+        public static string Lower(string s) => s.ToLower(); // BlitzMax conversion
+
+        public static string ExtractExt(string myFilePath){
+            var ret = System.IO.Path.GetExtension(myFilePath);
+            if (ret == "") { return ""; }
+            return Right(ret, ret.Length - 1);
+        }
+
+        public static string ExtractDir(string myFilePath) => System.IO.Path.GetDirectoryName(myFilePath);
+
+        public static bool Prefixed(string mystring, string prefix) => Left(mystring, prefix.Length) == prefix;
+        public static bool Suffixed(string mystring, string suffix) => Right(mystring, suffix.Length) == suffix;
+
+
     }
 }
