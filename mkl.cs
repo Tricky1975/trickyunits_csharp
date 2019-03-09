e06f4a2d56243d38d08402e401a6e665e30d1772
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-09 16:11:44 +0100

    Fix: Crash on non-existent license in "ALL" request

--- mkl.cs
+++ mkl.cs
@@ -1,42 +1,53 @@
 // Lic:
-//   mkl.cs
-//   MKL
-//   version: 18.09.27
-//   Copyright (C) 2018 Jeroen P. Broks
-//   This software is provided 'as-is', without any express or implied
-//   warranty.  In no event will the authors be held liable for any damages
-//   arising from the use of this software.
-//   Permission is granted to anyone to use this software for any purpose,
-//   including commercial applications, and to alter it and redistribute it
-//   freely, subject to the following restrictions:
-//   1. The origin of this software must not be misrepresented; you must not
-//      claim that you wrote the original software. If you use this software
-//      in a product, an acknowledgment in the product documentation would be
-//      appreciated but is not required.
-//   2. Altered source versions must be plainly marked as such, and must not be
-//      misrepresented as being the original software.
-//   3. This notice may not be removed or altered from any source distribution.
+// mkl.cs
+// TrickyUnits - MKL
+// version: 19.03.09
+// Copyright (C)  Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
 // EndLic
+
+
+
 using System;
 using System.Collections.Generic;
 
-namespace TrickyUnits
-{
+
+
+namespace TrickyUnits {
     /// <summary>MKL (MaKe License) was a system I set up for automatically adding license texts and version numbering. This way I can always be sure version changes never go without nummeric changes. It's a very simplistic system, but it does what it has to do. :)</summary>
     class MKL
     {
         static SortedDictionary<string, string> VERSIONS = new SortedDictionary<string, string>();
         static SortedDictionary<string, string> LICENSES = new SortedDictionary<string, string>();
 
+
+
         /// <summary>Defines version number</summary>
         static public void Version(string f, string v){
             VERSIONS[f] = v;
         }
 
+
+
         /// <summary>Defines license field</summary>
         static public void Lic(string f, string l){
+
             LICENSES[f] = l;
         }
+
+
 
         /// <summary>Shows all version information</summary>
         /// <remarks>A weakness in this system is that C# requires classes and that those classes are only loaded if actually needed, or rather the first time they are called, or created for a variable. Due to this version summaries can change if this function is called before all require classes are loaded.</remarks>
@@ -45,17 +56,20 @@ namespace TrickyUnits
             var dots = ""; for (var i = 0; i <= 40; i++) { dots += "."; }
             foreach(string k in VERSIONS.Keys){
                 var b = (k + " " + dots).Substring(0, 40)+" "+VERSIONS[k];
-                if (showlic) { b += " " + LICENSES[k]; } // TODO: Fix crash if license is not present!
+                if (showlic && LICENSES.ContainsKey(k)) { b += " " + LICENSES[k]; } 
                 ret += b + "\n";
             }
             return ret;
         }
 
-        static string Right(string s, int l = 1)
-        {
+
+
+        static string Right(string s, int l = 1) {
             if (l > s.Length) return s;
             return s.Substring(s.Length - l, l);
         }
+
+
 
         /// <summary>
         /// Shows you the 'newest version' based on the used sources
@@ -87,10 +101,13 @@ namespace TrickyUnits
             }
         }
 
+
         static MKL(){
-            MKL.Version("Tricky Units for C# - mkl.cs","18.09.27");
+            // Despite C# considering this as "obsolete" the "MKL." prefixes MUST be present here, or MKL_Update won't onderstand these values have to be updated.
+            MKL.Version("Tricky Units for C# - mkl.cs","19.03.09");
             MKL.Lic    ("Tricky Units for C# - mkl.cs","ZLib License");
         }
 
     }
 }
+
