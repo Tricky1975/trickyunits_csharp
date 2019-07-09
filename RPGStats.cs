d4779b5dc97735255f9480bd8b8ea94281ea4d24
Jeroen P. Broks <facebook@tbbs.nl>
2019-07-09 13:38:58 +0200

    License text added and set all line-breaks the TRUE way (read: Unix way)

--- RPGStats.cs
+++ RPGStats.cs
@@ -1,5 +1,31 @@
+// Lic:
+// RPGStats.cs
+// RPG Stat
+// version: 19.07.09
+// Copyright (C)  Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
+
 /* 
-    NOTES FROM THE ORIGINAL BLITZMAX CODE
+    LICENCE NOTES FROM THE ORIGINAL BLITZMAX CODE
+    Please note the MPL license is no longer valid, as I now replaced it 
+    with the zlib license (see above), the old license text was kept
+    for archiving's sake (and may be removed in the future).
+    If the BlitzMax version ever gets a serious update, the license
+    may be changed to zlib, as well!
+
 Rem
         RPGStats.bmx
 	(c) 2015, 2016, 2017, 2018 Jeroen Petrus Broks.
