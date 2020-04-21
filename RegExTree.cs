716107cabb3475430fb346f6b43c6fca4e2400b5
Jeroen P. Broks <facebook@tbbs.nl>
2020-04-21 17:03:48 +0200

    License

--- RegExTree.cs
+++ RegExTree.cs
@@ -1,3 +1,22 @@
+// Lic:
+// RegExTree.cs
+// RegExTree
+// version: 20.04.21
+// Copyright (C)  Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
 /* RegExTree
  * Please not when using this class the next classes MUST be present!
  * - Dirry
@@ -87,6 +106,13 @@ namespace TrickyUnits {
         }
 
         static public string[] TreeArray(string Expression, CheckCase CC = CheckCase.Auto, Type t = Type.FilesOnly, bool hidden = false) => Tree(Expression, CC, t, hidden).ToArray();
+
+        static public void Hello() { }
+
+        static RegExTree() {
+            MKL.Lic    ("Tricky Units for C# - RegExTree.cs","ZLib License");
+            MKL.Version("Tricky Units for C# - RegExTree.cs","20.04.21");
+        }
 
     }
 
